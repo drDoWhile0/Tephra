3cf6fe5ed39659065e19491dca2b03a5744c7af4
drDoWhile0 <your-email-for-drDoWhile0@example.com>
2026-06-08 00:10:52 -0600

    feature: begin laying the ground for the mapbox interactive map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="p-8">
-    <div v-if="loading" class="text-text-muted text-sm">Loading...</div>
-    <div v-else-if="error" class="text-danger text-sm">{{ error }}</div>
+    <div v-if="store.loading" class="text-text-muted text-sm">Loading...</div>
+    <div v-else-if="store.error" class="text-danger text-sm">{{ store.error }}</div>
     <div v-else class="text-text-muted text-sm">
-      {{ events.length }} events loaded
+      {{ store.filteredEvents.length }} events loaded
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { useUSGS } from '../composables/useUSGS'
+import { useVolcanoStore } from '../stores/useVolcanoStore'
 
-const { events, loading, error, fetchEvents } = useUSGS()
+const store = useVolcanoStore()
 
-// Vue equivalent of useEffect(() => { fetchEvents() }, [])
 onMounted(() => {
-  fetchEvents()
+  store.fetchEvents()
 })
 </script>
